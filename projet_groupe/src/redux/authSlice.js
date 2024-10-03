74e7965bcd2dbedb737cb5ff9eb29876e42cfcca
Chouaib <chouaibghd11@gmail.com>
2024-10-03 12:08:01 +0200

    correction

--- projet_groupe/src/redux/authSlice.js
+++ projet_groupe/src/redux/authSlice.js
@@ -1,38 +1,33 @@
-// src/features/authSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import MyAxios from '../MyAxios'; // Importer l'intercepteur MyAxios
+import MyAxios from '../MyAxios'; 
 
-// Action asynchrone pour la connexion
 export const loginUser = createAsyncThunk(
   'auth/loginUser',
   async (userCredentials, thunkAPI) => {
     const { username, password } = userCredentials;
     try {
-      // Utilisation de MyAxios pour faire l'appel API au backend
       const response = await MyAxios.post('/login', { username, password });
-      return response.data; // On suppose que la réponse contient { user, token }
+      return response.data; 
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data); // Gérer les erreurs retournées par l'API
+      return thunkAPI.rejectWithValue(error.response.data); 
     }
   }
 );
 
-// Action asynchrone pour l'inscription
 export const registerUser = createAsyncThunk(
   'auth/registerUser',
   async (userDetails, thunkAPI) => {
     const { username, password, email } = userDetails;
     try {
-      // Utilisation de MyAxios pour faire l'appel API au backend
       const response = await MyAxios.post('/register', { username, password, email });
-      return response.data; // On suppose que la réponse contient { user, token }
+      return response.data; 
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data); // Gérer les erreurs retournées par l'API
+      return thunkAPI.rejectWithValue(error.response.data); 
     }
   }
 );
 
-// Slice Redux pour gérer l'authentification
+
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
@@ -46,12 +41,11 @@ const authSlice = createSlice({
       state.user = null;
       state.token = null;
       state.status = 'idle';
-      localStorage.removeItem('token'); // Optionnel : Suppression du token lors de la déconnexion
+      localStorage.removeItem('token'); 
     },
   },
   extraReducers: (builder) => {
     builder
-      // Gestion de l'état pour login
       .addCase(loginUser.pending, (state) => {
         state.status = 'loading';
       })
@@ -59,13 +53,12 @@ const authSlice = createSlice({
         state.status = 'succeeded';
         state.user = action.payload.user;
         state.token = action.payload.token;
-        localStorage.setItem('token', action.payload.token); // Stocker le token dans localStorage
+        localStorage.setItem('token', action.payload.token); 
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.payload.message;
       })
-      // Gestion de l'état pour register
       .addCase(registerUser.pending, (state) => {
         state.status = 'loading';
       })
@@ -73,7 +66,7 @@ const authSlice = createSlice({
         state.status = 'succeeded';
         state.user = action.payload.user;
         state.token = action.payload.token;
-        localStorage.setItem('token', action.payload.token); // Stocker le token dans localStorage
+        localStorage.setItem('token', action.payload.token); 
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.status = 'failed';
